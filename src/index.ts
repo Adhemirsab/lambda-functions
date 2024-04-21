bb615ba9760bd03df4e7c4cd8d4101c706182b5c
Adhemir Sabino <adhemirsabino@gmail.com>
2024-04-21 13:43:18 -0500

    fix index.td

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-exports.handler = async (event: unknown): Promise<string> => {
+export const handler = async (event: unknown): Promise<string> => {
   console.log(event);
   return "Hello from Lambda!";
 };
